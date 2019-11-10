f9c9e273c84d7277b5ea377c6f655c7ca6f7d179
Sophie Scott-Maunder <sophiescottmaunder@gmail.com>
2019-11-11 07:16:41 +1300

    navbar draft

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,24 @@
+  
 import React from 'react'
+// import Product from './Product'
+// import Order from './Order'
+import { HashRouter as Router, Route } from 'react-router-dom'
+
+
+
+import NavbarPage from './NavbarPage'
+import HomePage from './Home'
 
 const App = () => {
   return (
-    <h1>React development has begun!</h1>
+    <div>
+      <Router>
+        <React.Fragment>
+          <NavbarPage />
+          <Route path='/' component={HomePage} />
+        </React.Fragment>
+      </Router>
+    </div>
   )
 }
 
---
+++ client/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HomePage = () => {
+    return (
+        <React.Fragment>
+            <div className='background'>
+                <h1>React development has begun!</h1>
+            </div>
+        </React.Fragment>
+
+    )
+}
+
+export default HomePage
