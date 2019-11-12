cd812c6cebe19d9726302a1729dc52e86dc03b64
Sophie Scott-Maunder <sophiescottmaunder@gmail.com>
2019-11-12 18:04:04 +1300

    update navbar with center logo

--- client/components/NavbarPage.jsx
+++ client/components/NavbarPage.jsx
@@ -1,73 +1,68 @@
 import React, { Component } from "react";
-import { MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBNavbarToggler, MDBCollapse, MDBDropdown,
-MDBDropdownToggle, MDBDropdownMenu, MDBDropdownItem, MDBContainer, MDBIcon } from "mdbreact";
+
+import {
+    MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBNavbarToggler, MDBCollapse, MDBDropdown,
+    MDBDropdownToggle, MDBDropdownMenu, MDBDropdownItem, MDBContainer, MDBIcon
+} from "mdbreact";
 
 class NavbarPage extends Component {
-state = {
-  collapseID: ""
-};
+    state = {
+        collapseID: ""
+    };
 
-toggleCollapse = collapseID => () =>
-  this.setState(prevState => ({
-  collapseID: prevState.collapseID !== collapseID ? collapseID : ""
-}));
+    toggleCollapse = collapseID => () =>
+        this.setState(prevState => ({
+            collapseID: prevState.collapseID !== collapseID ? collapseID : ""
+        }));
 
-render() {
-  return (
-    <MDBNavbar color="secondary-color" dark expand="md" style={{ marginTop: "20px" }}>
-    <MDBNavbarBrand>
-      <strong className="white-text">cocomo.</strong>
-    </MDBNavbarBrand>
-    <MDBNavbarToggler onClick={this.toggleCollapse("navbarCollapse3")} />
-    <MDBCollapse id="navbarCollapse3" isOpen={this.state.collapseID} navbar>
-      <MDBNavbarNav left>
-        <MDBNavItem active>
-          <MDBNavLink to="#!">Home</MDBNavLink>
-        </MDBNavItem>
-        <MDBNavItem>
-          <MDBNavLink to="#!">Features</MDBNavLink>
-        </MDBNavItem>
-        <MDBNavItem>
-          <MDBNavLink to="#!">Pricing</MDBNavLink>
-        </MDBNavItem>
-        <MDBNavItem>
-          <MDBDropdown>
-            <MDBDropdownToggle nav caret>
-              <div className="d-md-inline">MDBDropdown</div>
-            </MDBDropdownToggle>
-            <MDBDropdownMenu className="dropdown-default" right>
-              <MDBDropdownItem href="#!">Action</MDBDropdownItem>
-              <MDBDropdownItem href="#!">Another Action</MDBDropdownItem>
-              <MDBDropdownItem href="#!">Something else here</MDBDropdownItem>
-              <MDBDropdownItem href="#!">Something else here</MDBDropdownItem>
-            </MDBDropdownMenu>
-          </MDBDropdown>
-        </MDBNavItem>
-      </MDBNavbarNav>
-      <MDBNavbarNav right>
-        <MDBNavItem>
-          <MDBNavLink className="d-flex align-items-center" to="#!">1
-            <MDBIcon icon="envelope" className="ml-1" />
-          </MDBNavLink>
-        </MDBNavItem>
-        <MDBNavItem>
-          <MDBDropdown>
-            <MDBDropdownToggle className="dopdown-toggle" nav>
-              <img src="https://mdbootstrap.com/img/Photos/Avatars/avatar-2.jpg" className="rounded-circle z-depth-0"
-                style={{ height: "35px", padding: 0 }} alt="" />
-            </MDBDropdownToggle>
-            <MDBDropdownMenu className="dropdown-default" right>
-              <MDBDropdownItem href="#!">My account</MDBDropdownItem>
-              <MDBDropdownItem href="#!">Log out</MDBDropdownItem>
-            </MDBDropdownMenu>
-          </MDBDropdown>
-        </MDBNavItem>
-      </MDBNavbarNav>
-    </MDBCollapse>
-  </MDBNavbar>
-   
-    );
-  }
+    render() {
+        return (
+            <MDBNavbar color="secondary-color" dark expand="md" style={{ marginTop: "20px" }}>
+                <MDBNavbarBrand>
+                    <strong className="white-text">c</strong> <strong className="brown-text">.</strong>
+                </MDBNavbarBrand>
+                <MDBNavbarToggler onClick={this.toggleCollapse("navbarCollapse3")} />
+                <MDBCollapse id="navbarCollapse3" isOpen={this.state.collapseID} navbar>
+                    <MDBNavbarNav left>
+                        <MDBNavItem>
+                            <MDBDropdown>
+                                <MDBDropdownToggle nav caret>
+                                    <div className="d-md-inline">Products</div>
+                                </MDBDropdownToggle>
+                                <MDBDropdownMenu className="dropdown-default" right>
+                                    <MDBDropdownItem href="#!"></MDBDropdownItem>
+                                    <MDBDropdownItem href="#!">Chocolate Salami</MDBDropdownItem>
+                                    <MDBDropdownItem href="#!">Wedding Favours</MDBDropdownItem>
+                                    <MDBDropdownItem href="#!">Chocolate Coated Grapefruit Peel</MDBDropdownItem>
+                                </MDBDropdownMenu>
+                            </MDBDropdown>
+                        </MDBNavItem>
+                    </MDBNavbarNav>
+                    <MDBNavbarNav center="true">
+                        <MDBNavItem>
+                            <img src="/images/cocomo-brand.png" className="brand-image" alt="cocomo" />
+                        </MDBNavItem>
+                    </MDBNavbarNav>
+                    <MDBNavbarNav right>
+                        <MDBNavItem>
+                            <MDBNavLink to="#!">Contact Us</MDBNavLink>
+                        </MDBNavItem>
+                        <MDBNavItem>
+                            <MDBDropdown>
+                                <MDBDropdownToggle className="dropdown-toggle" nav>
+                                </MDBDropdownToggle>
+                                <MDBDropdownMenu className="dropdown-default" right>
+                                    <MDBDropdownItem href="#!">My account</MDBDropdownItem>
+                                    <MDBDropdownItem href="#!">Log out</MDBDropdownItem>
+                                </MDBDropdownMenu>
+                            </MDBDropdown>
+                        </MDBNavItem>
+                    </MDBNavbarNav>
+                </MDBCollapse>
+            </MDBNavbar>
+
+        );
+    }
 }
 
 export default NavbarPage;
